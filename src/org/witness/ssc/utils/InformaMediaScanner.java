cd35db8a8cba36807cdc72b0870edf4663f8b1ef
LvH <LvH@VIDEOCLASH-2.local>
2012-06-06 17:33:00 -0400

    (added custom media scanner handler)

---
+++ src/org/witness/ssc/utils/InformaMediaScanner.java
@@ -0,0 +1,47 @@
+package org.witness.ssc.utils;
+
+import java.io.File;
+
+import org.witness.informa.utils.InformaConstants;
+import org.witness.ssc.utils.ObscuraConstants.MediaScanner;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
+import android.net.Uri;
+import android.util.Log;
+
+public class InformaMediaScanner implements MediaScannerConnectionClient {
+	private MediaScannerConnection msc;
+	private File f;
+	private Context c;
+	
+	public InformaMediaScanner(Context c, File f) {
+		Log.d(InformaConstants.TAG, "Calling function has deferred to the Media Scanner before continuing");
+		Log.d(InformaConstants.TAG, "file: " + f.getAbsolutePath());
+		this.f = f;
+		this.c = c;
+		msc = new MediaScannerConnection(c, this);
+		msc.connect();
+	}
+	
+	public InformaMediaScanner(Context c, Uri u) {
+		this(c, new File(u.getPath()));
+	}
+
+	@Override
+	public void onMediaScannerConnected() {
+		msc.scanFile(f.getAbsolutePath(), null);
+	}
+
+	@Override
+	public void onScanCompleted(String path, Uri uri) {
+		msc.disconnect();
+		Log.d(InformaConstants.TAG, "new path: " + path + "\nnew uri for path: " + uri.toString());
+		c.sendBroadcast(new Intent()
+			.setAction(MediaScanner.SCANNED)
+			.putExtra(MediaScanner.URI, uri));
+	}
+
+}
