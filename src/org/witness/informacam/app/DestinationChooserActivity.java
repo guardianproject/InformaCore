49323cd65f8331532b26de0f00ec7f71779df2d6
LvH <LvH@VIDEOCLASH-2.local>
2012-08-06 10:11:06 -0400

    bringing destination chooser online...

--- src/org/witness/informacam/app/DestinationChooserActivity.java
+++ src/org/witness/informacam/app/DestinationChooserActivity.java
@@ -1,5 +1,47 @@
 package org.witness.informacam.app;
 
-public class DestinationChooserActivity {
+import java.util.ArrayList;
+
+import org.witness.informacam.R;
+import org.witness.informacam.app.mods.Selections;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class DestinationChooserActivity  extends Activity implements OnClickListener {
+	ListView keyChooser;
+	Button keyChooser_ok;
+	ArrayList<Selections> keys;
+	Handler h;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.destinationchooseractivity);
+		
+		keyChooser = (ListView) findViewById(R.id.keyChooser);
+		keyChooser_ok = (Button) findViewById(R.id.keyChooser_ok);
+		keyChooser_ok.setOnClickListener(this);
+		keys = new ArrayList<Selections>();
+		
+		h = new Handler();
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
