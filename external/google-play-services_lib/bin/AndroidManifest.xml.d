b53942bb8a2dc55a82e31219a30b533e24103b82
n8fr8 <nathan@freitas.net>
2013-07-01 16:52:18 -0400

    adding in this lib directly to external

---
+++ external/google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/n8fr8/dev/repos/iWitness/external/InformaCam/external/google-play-services_lib/bin/AndroidManifest.xml : \
+/home/n8fr8/dev/repos/iWitness/external/InformaCam/external/google-play-services_lib/AndroidManifest.xml \
