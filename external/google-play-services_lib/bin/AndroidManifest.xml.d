2461631c36b5598a199b04289d0eb36336464202
n8fr8 <nathan@freitas.net>
2013-07-02 09:38:43 -0400

    cleanup binary and gen folders

--- external/google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/n8fr8/dev/repos/iWitness/external/InformaCam/external/google-play-services_lib/bin/AndroidManifest.xml : \
-/home/n8fr8/dev/repos/iWitness/external/InformaCam/external/google-play-services_lib/AndroidManifest.xml \
