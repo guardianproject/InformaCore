c0d7354fa382a934a0b8bb8820bc5e5cb739a4de
harlo <LvH@VIDEOCLASH.local>
2013-10-09 16:43:20 -0400

    some updates to video region; still buggy, but usable.

--- src/org/witness/informacam/models/media/IVideoRegion.java
+++ src/org/witness/informacam/models/media/IVideoRegion.java
@@ -1,11 +1,23 @@
 package org.witness.informacam.models.media;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.annotation.Nullable;
 
 import org.witness.informacam.InformaCam;
+import org.witness.informacam.models.notifications.INotification;
 import org.witness.informacam.utils.Constants.IRegionDisplayListener;
 import org.witness.informacam.utils.Constants.Logger;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Iterables;
 
 import android.app.Activity;
 import android.util.Log;
@@ -56,6 +68,7 @@ public class IVideoRegion extends IRegion {
 	}
 	
 	public IRegionBounds getBoundsAtTimestampInQuestion() {
+		Log.d(LOG, "TIMESTAMP IN QUESTION: " + timestampInQuestion);
 		return getBoundsAtTime(timestampInQuestion);
 	}
 	
@@ -63,22 +76,71 @@ public class IVideoRegion extends IRegion {
 		timestampInQuestion = timestamp;
 	}
 	
-	public IRegionBounds getBoundsAtTime(long timestamp) {
+	public IRegionBounds getBoundsAtTime(final long timestamp) {
 		// TODO: TreeSet logic...
 		Log.d(LOG, "TIMESTAMP : " + timestamp);
 		Log.d(LOG, "startTime: " + bounds.startTime);
 		Log.d(LOG, "endTime: " + bounds.endTime);
 		
 		if(trail != null) {
-			int t = 0;
-			for(IVideoTrail v : trail) {
-				Log.d(LOG, "TRAIL:\n" + v.asJson().toString());
-				
-				if(v.timestamp == timestamp) {
-					return v.bounds;
+			// TODO: binary search would be better here...
+			
+			// if timestamp matches, then return that, otherwise
+			Collection<IVideoTrail> trailAtTime = Collections2.filter(trail, new Predicate<IVideoTrail>() {
+				@Override
+				public boolean apply(IVideoTrail videoTrail) {
+					return videoTrail.timestamp == timestamp;
 				}
-				
-				t++;
+			});
+			
+			try {
+				return trailAtTime.iterator().next().bounds;
+			} catch(NullPointerException e) {
+				Logger.d(LOG, "we didn't get an exact match... moving on...");
+				// otherwise...
+			} catch(NoSuchElementException e) {
+				Logger.d(LOG, "we didn't get an exact match... moving on...");
+				// otherwise...
+			}
+			
+			// get the subset (2) of objs whose timestamp is before ts, and after
+			IVideoTrail higher = null;
+			IVideoTrail lower = null;
+			
+			Collection<IVideoTrail> lowerEnd = Collections2.filter(trail, new Predicate<IVideoTrail>() {
+
+				@Override
+				public boolean apply(IVideoTrail videoTrail) {
+					return videoTrail.timestamp < timestamp;
+				}
+			});
+			Log.d(LOG, "lower end size: " + lowerEnd.size());
+			lower = Iterables.getLast(lowerEnd);
+			Log.d(LOG, "LOWER:\n" + lower.asJson().toString());
+			
+			Collection<IVideoTrail> higherEnd = Collections2.filter(trail, new Predicate<IVideoTrail>() {
+
+				@Override
+				public boolean apply(IVideoTrail videoTrail) {
+					return videoTrail.timestamp > timestamp;
+				}
+			});
+			try {
+				higher = higherEnd.iterator().next();
+			} catch(NoSuchElementException e) {
+				Logger.d(LOG, "nothing higher...");
+				return lower.bounds;
+			}
+			
+			// return the closest of those 2
+			try {
+				return Math.abs(higher.timestamp - timestamp) < Math.abs(lower.timestamp - timestamp) ? higher.bounds : lower.bounds;
+			} catch(NullPointerException e) {
+				if(higher != null) {
+					return higher.bounds;
+				} else if(lower != null) {
+					return lower.bounds;
+				}
 			}
 		}
 		
