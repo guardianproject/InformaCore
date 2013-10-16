391dbca766cde1baea3c4f4ad738ba11a227b4ff
harlo <LvH@VIDEOCLASH.local>
2013-10-16 16:46:45 -0400

    added some simple stubs for controlling informa

--- src/org/witness/informacam/informa/Cron.java
+++ src/org/witness/informacam/informa/Cron.java
@@ -2,25 +2,20 @@ package org.witness.informacam.informa;
 
 import org.witness.informacam.InformaCam;
 import org.witness.informacam.utils.Constants.App;
+import org.witness.informacam.utils.Constants.InformaCamEventListener;
+import org.witness.informacam.utils.InformaCamBroadcaster.InformaCamStatusListener;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
 
-public class Cron extends Service {
+public class Cron extends Service implements InformaCamEventListener, InformaCamStatusListener {
 	private final static String LOG = App.Background.LOG;
 	
 	InformaCam informaCam = null;
-	
-	/*
-	 * requires:
-	 * 
-	 *  access to iocipher
-	 *  access to informa
-	 *  
-	 */
 	
 	@Override
 	public void onCreate() {
@@ -33,6 +28,13 @@ public class Cron extends Service {
 		super.onStartCommand(intent, flags, startId);
 		Toast.makeText(this, "Cron.onStartCommand()", Toast.LENGTH_LONG).show();
 		Log.d(LOG, "Cron.onStartCommand()");
+		
+		informaCam = (InformaCam)getApplication();
+		
+		informaCam.setEventListener(this);
+		informaCam.setStatusListener(this);
+		
+		informaCam.startInforma();
 		
 		return START_STICKY;
 	}
@@ -49,11 +51,43 @@ public class Cron extends Service {
 		super.onDestroy();
 		Toast.makeText(this, "Cron.onDestroy()", Toast.LENGTH_LONG).show();
 		Log.d(LOG, "Cron.onDestroy()");
+		
+		informaCam.stopInforma();
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
+	}
+
+	@Override
+	public void onInformaCamStart(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInformaCamStop(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInformaStop(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInformaStart(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpdate(Message message) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
