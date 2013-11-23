caff57d76ce113e8d0e23143b29673a71a6cfe87
harlo <LvH@VIDEOCLASH.local>
2013-11-22 23:18:37 -0500

    minor fix to public credential upload

--- src/org/witness/informacam/utils/Constants.java
+++ src/org/witness/informacam/utils/Constants.java
@@ -474,7 +474,7 @@ public class Constants {
 				public final static String VIDEO = "video/mp4";
 				public final static String VIDEO_3GPP = "video/3gpp";
 				public static final String LOG = "informacam/log";
-				public static final String ZIP = "application/octet-stream";
+				public static final String ZIP = "application/zip";
 				public static final String ICTD = "application/octet-stream";
 				public static final String JSON = "application/json";
 			}
