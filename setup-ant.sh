5ae2fee62f9b847d76d36b714f69e1b4d69b518d
harlo <harlo.holmes@gmail.com>
2014-04-01 12:16:19 -0400

    changed array declaration because readarray is not universal

--- setup-ant.sh
+++ setup-ant.sh
@@ -5,13 +5,8 @@ if ! type -P android &> /dev/null; then
     echo "  Did you forget to setup the SDK?"
     exit 1
 fi
-readarray <<END
-external/android-ffmpeg-java
-external/OnionKit/libonionkit
-external/ODKFormParser
-external/CacheWord/cachewordlib
-external/google-play-services
-END
+
+declare -a MAPFILE=('external/android-ffmpeg-java' 'external/OnionKit/libonionkit' 'external/ODKFormParser' 'external/CacheWord/cachewordlib' 'external/google-play-services')
 
 for project in "${MAPFILE[@]}"; do
     android update lib-project --path $project -t android-17
