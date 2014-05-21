6ae02c7cd56da5a7d18d15978606941d524fbd46
n8fr8 <nathan@freitas.net>
2014-05-20 23:42:56 -0400

    pressure lookup using openweathermap API

--- src/org/witness/informacam/models/utils/PressureServiceUpdater.java
+++ src/org/witness/informacam/models/utils/PressureServiceUpdater.java
@@ -1,51 +1,30 @@
 package org.witness.informacam.models.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Locale;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.json.JSONObject;
 
 import android.hardware.SensorManager;
 import android.util.Log;
 
 public class PressureServiceUpdater {
 
-	 private float sea_level_pressure = SensorManager.PRESSURE_STANDARD_ATMOSPHERE;
+	 
 	  
-	  public static final String TAG_PRESSURE = "aws:pressure";
-	  public static final String TAG_LONGITUDE = "aws:longitude";
-	  public static final String TAG_LATITUDE = "aws:latitude";
-
-	  private final static String API_WXBUG = "TODO";
-	  
-	  public void updatePressure(final double longitude, final double latitude){
-	    Thread task = new Thread(new Runnable(){
-	      public void run() {
-	        sea_level_pressure = GetRefPressureWxbug(longitude,latitude);
-	        Log.d("Pressure","updated! " + sea_level_pressure);
-	      }});
-	    task.start();
-	  }  // this function is
-	  
-	  public float getSeaLevelPressure ()
-	  {
-		  return sea_level_pressure;
-	  }
-	  
-	  public float GetRefPressureWxbug(double longitude, double latitude) {
+	  public static float GetRefPressure(double longitude, double latitude) {
 	    InputStream is = null;
 	    float pressure = 0.0f;
 	    try {
-	      String strUrl = String.format(Locale.US,"http://" + API_WXBUG + ".api.wxbug.net/getLiveWeatherRSS.aspx?ACode=" + API_WXBUG + "&lat=%f&long=%f&UnitType=1&OutputType=1",latitude,longitude );
+	    	
+	    String strUrl = String.format(Locale.US, "http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f",latitude, longitude);
+	    	
+	    Log.d("PressureLookup","url: " + strUrl);
+	    
 	      URL text = new URL(strUrl);
 	      URLConnection connection = text.openConnection();
 	      connection.setReadTimeout(30000);
@@ -53,14 +32,30 @@ public class PressureServiceUpdater {
 
 	      is = connection.getInputStream();
 	      
-	      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	      DocumentBuilder domParser = dbf.newDocumentBuilder();
-	      Document xmldoc = domParser.parse(is);
-	      Element root = xmldoc.getDocumentElement();
+	      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	      int b = -1;
+	      while ((b = is.read())!=-1)
+	    	  baos.write(b);
 	      
-	      pressure = Float.parseFloat(getTagValue(TAG_PRESSURE, root));
-	      //float lon = Float.parseFloat(getTagValue(TAG_LONGITUDE, root));
-	      //float lat = Float.parseFloat(getTagValue(TAG_LATITUDE, root));
+	      String strJson = new String(baos.toByteArray());
+	      
+	      JSONObject json = new JSONObject(strJson);
+	      json = json.getJSONObject("main");
+	      
+	      pressure = (float)json.getDouble("pressure");
+	    //  pressure = (float)json.getDouble("sea_level");
+	      
+	      /**
+	       * {
+	       * "coord":{"lon":139,"lat":35},
+	       * "sys":{"message":0.1094,"country":"JP","sunrise":1400614568,"sunset":1400665503},
+	       * "weather":[{"id":501,"main":"Rain","description":"moderate rain","icon":"10d"}],
+	       * "base":"cmc stations",
+	       * "main":{"temp":289.125,"temp_min":289.125,"temp_max":289.125,"pressure":967.62,"sea_level":1009.99,"grnd_level":967.62,"humidity":100},"wind":{"speed":2.71,"deg":355.502},"rain":{"3h":4.25},"clouds":{"all":92},"dt":1400640069,"id":1851632,"name":"Shuzenji","cod":200}
+	       * 
+	       */
+	      
+
 	    } catch (Exception e) {
 	      Log.e("Pressure", "Error in network call", e);
 	      pressure = SensorManager.PRESSURE_STANDARD_ATMOSPHERE;
@@ -75,9 +70,5 @@ public class PressureServiceUpdater {
 	    return pressure;
 	  }
 	  
-	  private String getTagValue(String sTag, Element eElement) {
-	    NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
-	    Node nValue = (Node) nlList.item(0);
-	    return nValue.getNodeValue();
-	  }
+	  
 }
