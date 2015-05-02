2b274fae4940ef316060d33fefd954c9a8914921
n8fr8 <nathan@freitas.net>
2015-05-01 23:31:56 -0400

    remove IOCipher build from this project

--- build-native.sh
+++ build-native.sh
@@ -2,8 +2,8 @@ cd external/android-ffmpeg-java/external/android-ffmpeg/
 ./configure_make_everything.sh
 
 cd ../../../..
-make -C external/IOCipher/external/
-ndk-build -C external/IOCipher/
+#make -C external/IOCipher/external/
+#ndk-build -C external/IOCipher/
 
 ndk-build
 
