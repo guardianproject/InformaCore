f2d7ebd61b9aab7600b18d835f8a3386b7fe1874
n8fr8 <nathan@freitas.net>
2015-05-04 14:50:31 -0400

    update build-native script

--- build-native.sh
+++ build-native.sh
@@ -2,8 +2,6 @@ cd external/android-ffmpeg-java/external/android-ffmpeg/
 ./configure_make_everything.sh
 
 cd ../../../..
-#make -C external/IOCipher/external/
-#ndk-build -C external/IOCipher/
 
 ndk-build
 
