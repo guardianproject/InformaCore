c0822ff168f1027415c6f65c9b7b0b4b4c4ea2b5
n8fr8 <nathan@freitas.net>
2015-05-05 23:36:27 -0400

    remove cacheword from setup-ant script

--- setup-ant.sh
+++ setup-ant.sh
@@ -6,15 +6,14 @@ if ! type -P android &> /dev/null; then
     exit 1
 fi
 
-declare -a MAPFILE=('external/android-ffmpeg-java' 'external/OnionKit/libnetcipher' 'external/ODKFormParser' 'external/CacheWord/cachewordlib' 'external/google-play-services' 'external/CameraCipher')
+declare -a MAPFILE=('external/android-ffmpeg-java' 'external/OnionKit/libnetcipher' 'external/ODKFormParser' 'external/google-play-services' 'external/CameraCipher')
 
 for project in "${MAPFILE[@]}"; do
     android update lib-project --path $project -t android-19
 done
 
-cp external/CacheWord/cachewordlib/libs/iocipher.jar libs/iocipher.jar
-
-cp libs/android-support-v4.jar external/CacheWord/cachewordlib/libs/android-support-v4.jar
+#cp external/CacheWord/cachewordlib/libs/iocipher.jar libs/iocipher.jar
+#cp libs/android-support-v4.jar external/CacheWord/cachewordlib/libs/android-support-v4.jar
 cp libs/android-support-v4.jar external/OnionKit/libnetcipher/libs/android-support-v4.jar
 
 android update project --path . -t android-19 -n InformaCamCore --subprojects
