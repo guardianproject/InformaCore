ef0f730239e58699802c9381fb0c307ba9d0a005
n8fr8 <nathan@freitas.net>
2015-05-13 23:31:51 -0400

    add partial wakelock to server so it works with screen off

--- src/org/witness/informacam/share/WebShareService.java
+++ src/org/witness/informacam/share/WebShareService.java
@@ -18,8 +18,11 @@ import org.witness.informacam.utils.Constants.Models;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import ch.boye.httpclientandroidlib.conn.util.InetAddressUtils;
@@ -36,6 +39,11 @@ public class WebShareService extends Service {
 	
 	private String[] mMediaList = null;
 	
+
+	WakeLock mWakeLock;
+	private final static String TAG = "WebShareService";
+	
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
@@ -48,6 +56,15 @@ public class WebShareService extends Service {
 		super.onCreate();
 		
 		
+	}
+
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		
+		if (mWakeLock != null && mWakeLock.isHeld())
+			mWakeLock.release();
 	}
 
 
@@ -82,7 +99,7 @@ public class WebShareService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		
-		if (intent.getAction() != null)
+		if (intent != null && intent.getAction() != null)
 		{
 
 			if (intent.getAction().equals(ACTION_SERVER_START))
@@ -119,6 +136,10 @@ public class WebShareService extends Service {
 			
 			public void run ()
 			{
+				final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+		        mWakeLock.acquire();
+		        
 				mServer = new SimpleWebServer(mHost,mPort,mRoot,false);
 				showNotification ();
 				initMediaShare();
@@ -145,6 +166,9 @@ public class WebShareService extends Service {
 			
 			public void run ()
 			{
+				if (mWakeLock != null && mWakeLock.isHeld())
+					mWakeLock.release();
+				
 				stopForeground(true);
 				
 				try {
@@ -166,8 +190,10 @@ public class WebShareService extends Service {
 		return (mServer != null && mServer.isAlive());
 	}
 
-	public static String getLocalIpAddress(){
+	public static String[] getLocalIpAddresses(){
 		   try {
+			   ArrayList<String> alAddresses = new ArrayList<String>();
+			   
 		       for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();  
 		       en.hasMoreElements();) {
 		       NetworkInterface intf = en.nextElement();
@@ -175,12 +201,15 @@ public class WebShareService extends Service {
 		           InetAddress inetAddress = enumIpAddr.nextElement();
 		                if (!inetAddress.isLoopbackAddress()&& InetAddressUtils.isIPv4Address(inetAddress.getHostAddress())) {
 		                	
-		                	return inetAddress.getHostAddress();
+		                	alAddresses.add(inetAddress.getHostAddress());
 		                
 		                
 		                }
 		           }
 		       }
+		       
+		       return alAddresses.toArray(new String[alAddresses.size()]);
+		       
 		       } catch (Exception ex) {
 		          Log.e("IP Address", ex.toString());
 		      }
